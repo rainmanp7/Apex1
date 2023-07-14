284e7572a36a13ed636a7be36dc70a5a09b39af5
Area15 R&D <muslimsoap@gmail.com>
2023-07-14 22:50:43 +0800

    Update apex.c
    More professional with error control

--- apex.c
+++ apex.c
@@ -1,51 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <math.h>
+#include <time.h>
+struct MachineLearningModule {
+char *file_name;
+void (*initialize)(struct MachineLearningModule *self);
+int (*load_model)(struct MachineLearningModule *self, const char
+*file_name);
+};
 int main() {
 // Create the 3 virtual memory files.
-char *congestion_status_file;
-char *ttl_file;
-char *current_congestion_window_status_file;
-int mmap_result = mmap(NULL, 476160, PROT_READ | PROT_WRITE,
+char *congestion_status_file = mmap(NULL, 476160, PROT_READ |
+PROT_WRITE,
+MAP_PRIVATE | MAP_ANONYMOUS, -1,
+0);
+char *ttl_file = mmap(NULL, 476160, PROT_READ | PROT_WRITE,
 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-if (mmap_result == MAP_FAILED) {
-printf("Failed to map files\n");
-return 1;
-}
-congestion_status_file = (char *)mmap_result;
-mmap_result = mmap(NULL, 476160, PROT_READ | PROT_WRITE,
-MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-if (mmap_result == MAP_FAILED) {
-printf("Failed to map files\n");
-return 1;
-}
-ttl_file = (char *)mmap_result;
-mmap_result = mmap(NULL, 476160, PROT_READ | PROT_WRITE,
-MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-if (mmap_result == MAP_FAILED) {
-printf("Failed to map files\n");
-return 1;
-}
-current_congestion_window_status_file = (char *)mmap_result;
+char *current_congestion_window_status_file = mmap(
+NULL, 476160, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS,
+-1, 0);
 // Create the 3 machine learning modules.
-MachineLearningModule *congestion_status_module =
-malloc(sizeof(MachineLearningModule));
-MachineLearningModule *ttl_module =
-malloc(sizeof(MachineLearningModule));
-MachineLearningModule *current_congestion_window_status_module =
-malloc(sizeof(MachineLearningModule));
-if (congestion_status_module == NULL || ttl_module == NULL ||
-current_congestion_window_status_module == NULL) {
-printf("Failed to allocate memory for machine learning modules\n");
-return 1;
-}
-congestion_status_module->initialize(congestion_status_file);
-ttl_module->initialize(ttl_file);
-current_congestion_window_status_module->initialize(
-current_congestion_window_status_file);
+struct MachineLearningModule *congestion_status_module =
+malloc(sizeof(struct MachineLearningModule));
+struct MachineLearningModule *ttl_module =
+malloc(sizeof(struct MachineLearningModule));
+struct MachineLearningModule *current_congestion_window_status_module =
+malloc(sizeof(struct MachineLearningModule));
+// Initialize the machine learning modules.
+congestion_status_module->file_name = "congestion_status_model.pkl";
+congestion_status_module->initialize =
+&MachineLearningModule::initialize;
+congestion_status_module->load_model =
+&MachineLearningModule::load_model;
+ttl_module->file_name = "ttl_model.pkl";
+ttl_module->initialize = &MachineLearningModule::initialize;
+ttl_module->load_model = &MachineLearningModule::load_model;
+current_congestion_window_status_module->file_name =
+"current_congestion_window_status_model.pkl";
+current_congestion_window_status_module->initialize =
+&MachineLearningModule::initialize;
+current_congestion_window_status_module->load_model =
+&MachineLearningModule::load_model;
 // Load the machine learning models from files.
-if (congestion_status_module->load_model("congestion_status_model.pkl")
-!= 0 ||
-ttl_module->load_model("ttl_model.pkl") != 0 ||
+if (congestion_status_module->load_model(congestion_status_file) != 0 ||
+ttl_module->load_model(ttl_file) != 0 ||
 current_congestion_window_status_module->load_model(
-"current_congestion_window_status_model.pkl") != 0) {
+current_congestion_window_status_file) != 0) {
 printf("Failed to load machine learning models\n");
 return 1;
 }
