124aa328aa86bae27fa77d5b445042e853f43459
Area15 R&D <muslimsoap@gmail.com>
2023-07-14 22:33:17 +0800

    Update apex.c
    The while (1) loop will continuously train the machine learning modules and improve
    their predictions. This means that the predictions from the machine learning modules
    will become more accurate over time, so there is no need to get the predictions from
    the modules every time.

--- apex.c
+++ apex.c
@@ -1,84 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/mman.h>
-#include <math.h>
-#include <time.h>
-struct MachineLearningModule {
-NeuralNetwork *model;
-void initialize(char *file) {
-model = load_neural_network(file);
-}
-void train() {
-train_neural_network(model);
-}
-void run() {
-model->run();
-}
-float predict() {
-return model->predict();
-}
-};
-struct Connection {
-int congestion_window_size;
-MachineLearningModule *congestion_status_module;
-MachineLearningModule *ttl_module;
-MachineLearningModule *current_congestion_window_status_module;
-};
 int main() {
 // Create the 3 virtual memory files.
 char *congestion_status_file;
 char *ttl_file;
 char *current_congestion_window_status_file;
-congestion_status_file = mmap(NULL, 476160, PROT_READ | PROT_WRITE,
+int mmap_result = mmap(NULL, 476160, PROT_READ | PROT_WRITE,
 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-ttl_file = mmap(NULL, 476160, PROT_READ | PROT_WRITE, MAP_PRIVATE |
-MAP_ANONYMOUS, -1, 0);
-current_congestion_window_status_file = mmap(NULL, 476160, PROT_READ |
-PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-if (congestion_status_file == MAP_FAILED || ttl_file == MAP_FAILED ||
-current_congestion_window_status_file == MAP_FAILED) {
+if (mmap_result == MAP_FAILED) {
 printf("Failed to map files\n");
 return 1;
 }
+congestion_status_file = (char *)mmap_result;
+mmap_result = mmap(NULL, 476160, PROT_READ | PROT_WRITE,
+MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+if (mmap_result == MAP_FAILED) {
+printf("Failed to map files\n");
+return 1;
+}
+ttl_file = (char *)mmap_result;
+mmap_result = mmap(NULL, 476160, PROT_READ | PROT_WRITE,
+MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+if (mmap_result == MAP_FAILED) {
+printf("Failed to map files\n");
+return 1;
+}
+current_congestion_window_status_file = (char *)mmap_result;
 // Create the 3 machine learning modules.
-MachineLearningModule *congestion_status_module;
-MachineLearningModule *ttl_module;
-MachineLearningModule *current_congestion_window_status_module;
-congestion_status_module = malloc(sizeof(MachineLearningModule));
-ttl_module = malloc(sizeof(MachineLearningModule));
-current_congestion_window_status_module =
+MachineLearningModule *congestion_status_module =
 malloc(sizeof(MachineLearningModule));
-// Initialize the 3 machine learning modules.
+MachineLearningModule *ttl_module =
+malloc(sizeof(MachineLearningModule));
+MachineLearningModule *current_congestion_window_status_module =
+malloc(sizeof(MachineLearningModule));
+if (congestion_status_module == NULL || ttl_module == NULL ||
+current_congestion_window_status_module == NULL) {
+printf("Failed to allocate memory for machine learning modules\n");
+return 1;
+}
 congestion_status_module->initialize(congestion_status_file);
 ttl_module->initialize(ttl_file);
-current_congestion_window_status_module->initialize(current_congestion_win
-dow_status_file);
+current_congestion_window_status_module->initialize(
+current_congestion_window_status_file);
 // Load the machine learning models from files.
-congestion_status_module->load_model("congestion_status_model.pkl");
-ttl_module->load_model("ttl_model.pkl");
-current_congestion_window_status_module->load_model("current_congestion_wi
-ndow_status_model.pkl");
-// Create a connection.
-struct Connection connection;
-connection.congestion_window_size = 10;
-connection.congestion_status_module = congestion_status_module;
-connection.ttl_module = ttl_module;
-connection.current_congestion_window_status_module =
-current_congestion_window_status_module;
-// Get the predictions from the 3 machine learning modules.
-float congestion_status_prediction =
-connection.congestion_status_module->predict();
-float ttl_prediction = connection.ttl_module->predict();
-float current_congestion_window_status_prediction =
-connection.current_congestion_window_status_module->predict();
-// Adjust the congestion window size based on the predictions.
-if (congestion_status_prediction < 0.5) {
-// The network is congested.
-connection.congestion_window_size = connection.congestion_window_size / 2;
-} else {
-// The network is not congested.
-connection.congestion_window_size = connection.congestion_window_size * 2;
+if (congestion_status_module->load_model("congestion_status_model.pkl")
+!= 0 ||
+ttl_module->load_model("ttl_model.pkl") != 0 ||
+current_congestion_window_status_module->load_model(
+"current_congestion_window_status_model.pkl") != 0) {
+printf("Failed to load machine learning models\n");
+return 1;
 }
-// Close the 3 virtual memory files.
-munmap(congestion_status_file, 476160);
-munmap(ttl_file, 476160);
+// Start the training loop.
+int epoch = 0;
+while (1) {
+// Train the machine learning modules.
+congestion_status_module->train();
+ttl_module->train();
+current_congestion_window_status_module->train();
+// Increment the epoch counter.
+epoch++;
+}
+return 0;
+}
