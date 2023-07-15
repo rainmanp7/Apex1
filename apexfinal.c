9fd81fab56a9fc154eb4a2ac0866a16ec30b75eb
Area15 R&D <muslimsoap@gmail.com>
2023-07-15 22:01:10 +0800

    Update apexfinal.c
    multiple changes have been made to reduce acknowledgements.

--- apexfinal.c
+++ apexfinal.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 unsigned long congestion_control(unsigned long value,
 size_t packet_size) {
 unsigned long congestion_window_size = value * 2;
@@ -34,67 +36,12 @@ if (congestion_type == NORMAL) {
 // Use a more sophisticated congestion control algorithm.
 double round_trip_time = get_round_trip_time();
 double bandwidth = get_bandwidth();
-new_congestion_window = new_congestion_window * (1 + round_trip_time /100 +
-bandwidth / 100000);
-} else if (congestion_type == CONGESTION_AVOIDANCE) {
-// Consider other factors that affect congestion.
-new_congestion_window = new_congestion_window / 2 +
-(get_number_of_connections() / 100);
-} else if (congestion_type == DISTURBED) {
-// Handle disturbed connections.
-// Instead of dividing the congestion window size by 4,
-// we can use a more sophisticated approach
-// that takes into account the severity of the disturbance.
-double disturbance_severity = get_disturbance_severity();
-new_congestion_window = new_congestion_window * (1 -
-disturbance_severity);
-}
-// Update the congestion window size for each connection.
-for (Connection connection : connections) {
-connection.congestion_window = new_congestion_window;
-}
-return new_congestion_window;
-}
-// Calculate the round-trip time.
-unsigned long get_round_trip_time() {
-// Get the round-trip time from the network.
-double round_trip_time = 100;
-return round_trip_time;
-}
-// Calculate the bandwidth of the network.
-unsigned long get_bandwidth() {
-// Get the bandwidth from the network.
-double bandwidth = 100000;
-return bandwidth;
-}
-// Calculate the severity of the disturbance.
-double get_disturbance_severity() {
-// Get the severity of the disturbance from the network.
-double disturbance_severity = 0.5;
-return disturbance_severity;
-}
-packet_size);
-// Handle different types of congestion.
-enum CongestionType {
-NORMAL,
-SLOW_START,
-CONGESTION_AVOIDANCE,
-DISTURBED,
-};
-CongestionType congestion_type = NORMAL;
-if (has_packet_dropped()) {
-congestion_type = CONGESTION_AVOIDANCE;
-} else if (is_connection_disturbed()) {
-congestion_type = DISTURBED;
-}
-// Adjust the congestion window size.
-if (congestion_type == NORMAL) {
-// Use a more sophisticated congestion control algorithm.
-double round_trip_time = get_round_trip_time();
-double bandwidth = get_bandwidth();
 new_congestion_window = new_congestion_window * (1 + round_trip_time /
 100 +
 bandwidth / 100000);
+// Only acknowledge packets that have not been lost.
+if not random.random() < get_loss_rate()):
+new_congestion_window += 1
 } else if (congestion_type == CONGESTION_AVOIDANCE) {
 // Consider other factors that affect congestion.
 new_congestion_window = new_congestion_window / 2 +
@@ -132,4 +79,3 @@ double get_disturbance_severity() {
 double disturbance_severity = 0.5;
 return disturbance_severity;
 }
-
